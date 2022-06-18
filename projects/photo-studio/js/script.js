994f087bde63ae2a8b3d0d2b8ab7cfdc8ae5f59d
Dmytro <dmytropopov7@gmail.com>
2022-06-18 15:32:11 +0300

    - Add 4 photo - Comment some bugs in code

--- projects/photo-studio/js/script.js
+++ projects/photo-studio/js/script.js
@@ -16,3 +16,57 @@ menuToggler.addEventListener("click",()=>{
 })
 
 console.log(headerMenuRight.classList.contains("show-flex"));
+
+
+// const tabContentHumansImg = document.querySelectorAll(".professional__tabcontent_human img"),
+//       tabContentRightPreview = document.querySelectorAll(".tabcontent__right_img"),
+//       tabContent = document.querySelectorAll(".tabContent");
+// let tabContents = Array.prototype.slice.call(tabContent);
+// console.log(tabContents)
+
+// function hideAllPhotos(selector){
+//     const elem = document.querySelectorAll(selector);
+//     elem.forEach(item=>{
+//         item.classList.remove('show');
+//         item.classList.add('hide');
+//     });
+// }
+
+// function showPhoto(i = 0, selector){
+//     const elem = document.querySelectorAll(selector);
+//     elem[i].classList.remove('hide');
+//     elem[i].classList.add('show');
+// };
+
+// tabContentHumansImg.forEach((item,i)=>{
+//     item.addEventListener('click',()=>{
+//         hideAllPhotos(".tabcontent__right_img");
+//         showPhoto(i, '.tabcontent__right_img');
+//     });
+// });
+
+// hideAllPhotos(".tabcontent__right_img");
+// showPhoto(0, ".tabcontent__right_img");
+// console.log(tabContent)
+// function showSlides(rightArrow){
+    
+//     rightArrow.addEventListener("click",()=>{
+        
+//         tabContents.forEach((item,i)=>{
+//             item.classList.remove('hide');
+//             item.classList.add('show');
+//             console.log(i)
+//         });
+//     });
+    
+// }
+
+// const arrowRight = document.querySelector("#arrow_right")
+
+// showSlides(arrowRight);
+
+
+// if(document.body.offsetWidth<576){
+//     hideAllPhotos('.tabContent');
+//     showPhoto(0,".tabContent")
+// }
