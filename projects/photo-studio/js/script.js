31c957df7999e287597dad5c4849585d490ca5cd
Dmytro <dmytropopov7@gmail.com>
2022-06-23 17:46:52 +0300

    - Created Slider with reviews - Fix bugs

--- projects/photo-studio/js/script.js
+++ projects/photo-studio/js/script.js
@@ -70,3 +70,37 @@ console.log(headerMenuRight.classList.contains("show-flex"));
 //     hideAllPhotos('.tabContent');
 //     showPhoto(0,".tabContent")
 // }
+
+
+
+// Reviews Slider
+
+$('.reviews__slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll:1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
